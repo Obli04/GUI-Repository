7dc030fb2be376cd64624ce0bd2f30d7af9c45ba
Obli04 <scacciadavide12@gmail.com>
2025-01-15 17:21:47 +0100

    Fixed DatabaseMigration warnings

--- e-wallet/src/main/java/beans/databaseUtility/DatabaseMigration.java
+++ e-wallet/src/main/java/beans/databaseUtility/DatabaseMigration.java
@@ -1,9 +1,11 @@
 package beans.databaseUtility;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -30,15 +32,13 @@ public class DatabaseMigration {
 
         try {
             Class.forName("org.postgresql.Driver"); //Load the PostgreSQL JDBC Driver
-            Connection connection = DriverManager.getConnection(url, user, password); //Establish a connection to the database
-            Statement statement = connection.createStatement(); //Create a statement object to execute SQL commands
-            String sql = new String(Files.readAllBytes(Paths.get("src/main/resources/db/migration/V7_Remove_User.sql"))); //Read the SQL script from the file
-            statement.execute(sql); //Execute the SQL script
-            System.out.println("Database tables updated successfully!"); //Print a message to the console
-            statement.close(); //Close the statement object
-            connection.close(); //Close the connection to the database
-        } 
-        catch (Exception e) {
+            try (Connection connection = DriverManager.getConnection(url, user, password); //Establish a connection to the database
+                 Statement statement = connection.createStatement()) { //Create a statement object to execute SQL commands
+                String sql = new String(Files.readAllBytes(Paths.get("src/main/resources/db/migration/V7_Remove_User.sql"))); //Read the SQL script from the file
+                statement.execute(sql); //Execute the SQL script
+                System.out.println("Database tables updated successfully!"); //Print a message to the console
+            } //Close the statement object
+        } catch (IOException | ClassNotFoundException | SQLException e) {
             System.err.println("Error during database migration: " + e.getMessage()); //Catch any exceptions printing the error message
         }
     }
