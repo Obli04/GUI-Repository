e70e7518abe76675426178e754a42364e94e2e89
Obli04 <scacciadavide12@gmail.com>
2025-01-19 15:00:00 +0100

    Fixes to deletingAccount

--- e-wallet/src/main/resources/db/migration/V6_Final_Database.sql
+++ e-wallet/src/main/resources/db/migration/V6_Final_Database.sql
@@ -50,3 +50,11 @@ CREATE TABLE requestMoney (
     FOREIGN KEY (id_sender) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (id_receiver) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE friends (
+    id SERIAL PRIMARY KEY,
+    id_user_1 INTEGER NOT NULL,
+    id_user_2 INTEGER NOT NULL,
+    FOREIGN KEY (id_user_1) REFERENCES users(id),
+    FOREIGN KEY (id_user_2) REFERENCES users(id)
+);
